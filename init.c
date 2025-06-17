1f4423061304c5cc9ca5460069263073f637e8be
CodeSorting <jb102903030329@gmail.com>
2025-06-17 17:29:34 +0900

    처음 선택창

---
+++ init.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+char print[200] = "==============================\n\tZOMBIE SURVIVAL\n==============================\n   1. New Game\n   2. Load Game\n   3. Options\n   4. Exit\n==============================\nPlease select: ";
+int main() {
+	printf("%s",print);
+}
