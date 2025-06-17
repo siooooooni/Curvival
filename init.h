4e1ed5f7672f41b3b9206a5b802472acb1cecdd9
CodeSorting <jb102903030329@gmail.com>
2025-06-17 18:18:08 +0900

    1

--- init.h
+++ init.h
@@ -1,6 +1,9 @@
+#ifndef INIT_H
+#define INIT_H
 #include <stdio.h>
 #include <string.h>
 char print[200] = "==============================\n\tZOMBIE SURVIVAL\n==============================\n   1. New Game\n   2. Load Game\n   3. Options\n   4. Exit\n==============================\nPlease select: ";
-int main() {
+void init_print() {
 	printf("%s",print);
 }
+#endif
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "init.h"
+
+int main() {
+	init_print();
+}
