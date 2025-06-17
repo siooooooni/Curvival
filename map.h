c0eba4ae1980e68af9ebf5941c4d2ab72491631d
CodeSorting <jb102903030329@gmail.com>
2025-06-18 00:47:33 +0900

    배경 2차 업데이트. 건물 고정 or 랜덤으로 할지 고민 필요

--- map.h
+++ map.h
@@ -4,24 +4,72 @@
 #include <stdio.h>
 #include <string.h>
 #include <ncurses.h>
+#include <time.h>
 
 #define MAP_HEIGHT 50
 #define MAP_WIDTH 100
+#define HOUSE_COUNT 7
+#define ZOMBIE_MAX 20
+#define ZOMBIE_MIN 15
+#define ITEM_COUNT 3
 //chtype map[MAP_HEIGHT][MAP_WIDTH];
+enum Object {EMPTY = '.',WALL = '#', ZOMBIE = 'z', PLAYER = '8', ITEM = '!'};
 void init_map(chtype map[MAP_HEIGHT][MAP_WIDTH]) {
     for (int i=0;i<MAP_HEIGHT;++i) {
         for (int j=0;j<MAP_WIDTH;++j) {
             map[i][j] = '.' | COLOR_PAIR(1); //빈칸
         }
     }
-    for (int i=0;i<MAP_WIDTH;++i) map[0][i] = '#' | COLOR_PAIR(2); //벽
-    for (int i=0;i<MAP_HEIGHT;++i) map[i][0] = '#' | COLOR_PAIR(2);
-    for (int i=0;i<MAP_WIDTH;++i) map[MAP_HEIGHT-1][i] = '#' | COLOR_PAIR(2);
-    for (int i=0;i<MAP_HEIGHT;++i) map[i][MAP_WIDTH-1] = '#' | COLOR_PAIR(2);
-    map[10][10] = '8' | COLOR_PAIR(3) | A_BOLD; //플레이어
-    map[48][30] = 'z' | COLOR_PAIR(4); //좀비
-    map[30][30] = '!' | COLOR_PAIR(5) | A_BLINK; //아이템
-
+    for (int i=0;i<MAP_WIDTH;++i) map[0][i] = WALL | COLOR_PAIR(2); //벽
+    for (int i=0;i<MAP_HEIGHT;++i) map[i][0] = WALL | COLOR_PAIR(2);
+    for (int i=0;i<MAP_WIDTH;++i) map[MAP_HEIGHT-1][i] = WALL | COLOR_PAIR(2);
+    for (int i=0;i<MAP_HEIGHT;++i) map[i][MAP_WIDTH-1] = WALL | COLOR_PAIR(2);
+	//건물 4개 생성 (직사각형)
+	srand(time(NULL));
+	int w,h,ry,rx;
+	h = 6; w = 12; //중앙 건물
+	ry = MAP_HEIGHT/2 - h/2;
+	rx = MAP_WIDTH/2 - w/2;
+	for (int i=ry;i<ry+h;++i) {
+		for (int j=rx;j<rx+w;++j) {
+			if (i==ry || i==ry+h-1 || j==rx || j==rx+w-1) map[i][j] = WALL | COLOR_PAIR(2);
+		}
+	}
+	// 나머지 3개 건물 (랜덤 위치 및 크기)
+    for (int b=1;b<HOUSE_COUNT;++b) {
+        h = rand() % 5 + 5;        // 높이 4~7
+        w = rand() % 12 + 5;        // 너비 6~13
+        ry = rand() % (MAP_HEIGHT - h - 2) + 1;
+        rx = rand() % (MAP_WIDTH  - w - 2) + 1;
+        for (int i=ry;i<ry+h;++i) {
+            for (int j=rx;j<rx+w;++j) {
+                if (i==ry || i==ry+h-1 || j==rx || j==rx+w-1)
+                    map[i][j] = WALL | COLOR_PAIR(2);
+            }
+        }
+    }
+	//플레이어 위치
+	map[25][50] = '8' | COLOR_PAIR(3) | A_BOLD; //플레이어
+    
+	//좀비 랜덤 생성 : ZOMBIE_MIN ~ ZOMBIE_MAX
+	int zombieCount = rand() % (ZOMBIE_MAX - ZOMBIE_MIN + 1) + ZOMBIE_MIN;
+	for (int z=0;z<zombieCount;++z) {
+        int y,x;
+        do {
+            y = rand() % (MAP_HEIGHT - 2) + 1; //벽은 제외
+            x = rand() % (MAP_WIDTH  - 2) + 1;
+        } while (map[y][x] != (EMPTY | COLOR_PAIR(1))); //빈 공간이어야함.
+        map[y][x] = ZOMBIE | COLOR_PAIR(4);
+    }
+	//아이템 랜덤 생성 : ITEM_COUNT
+    for (int it=0;it<ITEM_COUNT;++it) {
+        int y,x;
+        do {
+            y = rand() % (MAP_HEIGHT - 2) + 1;
+            x = rand() % (MAP_WIDTH  - 2) + 1;
+        } while (map[y][x] != (EMPTY | COLOR_PAIR(1)));
+        map[y][x] = ITEM | COLOR_PAIR(5) | A_BLINK;
+    }
 }
 void draw_map(chtype map[MAP_HEIGHT][MAP_WIDTH]) {
     for (int i=0;i<MAP_HEIGHT;++i) {
