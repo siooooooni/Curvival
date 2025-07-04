98060f8c06211746a1e7db5f387e8c947145388c
CodeSorting <jb102903030329@gmail.com>
2025-07-04 17:56:03 +0900

    폴더 정리

--- div/player.h
+++
@@ -1,8 +0,0 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-
-#include "game.h"
-
-void HumanMove(human *player, int way); //플레이어 이동
-
-#endif // PLAYER_H 
--- div/ui.h
+++
@@ -1,8 +0,0 @@
-#ifndef UI_H
-#define UI_H
-
-#include "game.h"
-
-void init_UI(); //시작 UI
-
-#endif // UI_H 
